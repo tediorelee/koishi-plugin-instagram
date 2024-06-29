f9029c138e77b2aba980e5c3ef9c396cea10ebe4
tediorelee <tediorelee@leejieun.fan>
2024-06-29 15:06:14 +0800

    feat: init

--- readme.md
+++ readme.md
@@ -3,3 +3,15 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-instagram?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-instagram)
 
 Koishi plugin for instagram
+
+使用RapidAPI为ig图片/视频内容解析
+
+注册账号：https://rapidapi.com/auth/sign-up
+
+点击屏幕正上方"Search APIs"搜索"Instagram"
+
+找到Instagram Looter，打开后点击右侧"Subscribe to test"
+
+选择"Start Free Plan" - "Subscribe"
+
+记录下你的"x-rapidapi-key"填入配置
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'instagram'
 
-export interface Config {}
+export interface Config {
+  host: string,
+  key: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config = Schema.object({
+  host: Schema.string().default('instagram-looter2.p.rapidapi.com').description('不需要做任何改动'),
+  key: Schema.string().default('').description('填写从rapidapi获取的key'),
+  description: Schema.string().default('请看readme教程获取对应的key').description(''),
+})
 
-export function apply(ctx: Context) {
+export const GRAPH_SIDECAR = 'GraphSidecar';
+export const GRAPH_IMAGE = 'GraphImage';
+export const GRAPH_VIDEO = 'GraphVideo';
+
+export function apply(ctx: Context, config: Config) {
   // write your plugin here
+
+  async function fetchFromAPI(url: string) {
+    const headers = {
+      'X-RapidAPI-Key': config.key,
+      'X-RapidAPI-Host': config.host
+    };
+    return await ctx.http.get('https://' + config.host + '/post?link=' + url, { headers });
+  }
+
+  ctx.middleware(async (session, next) => {
+    if (!session.content.includes('instagram.com')) return next()
+
+    try {
+      const result = await fetchFromAPI(session.content);
+      const {
+        status,
+        is_video,
+        __typename
+      } = result
+
+      if (status !== true) {
+        return '解析失败!';
+      } else {
+        switch (__typename) {
+          case GRAPH_SIDECAR:
+            const { edges } = result.edge_sidecar_to_children
+            edges.forEach(async item => {
+              if (item.node.is_video) {
+                await session.send(h.video(item.node.video_url))
+              } else {
+                await session.send(h.image(item.node.display_url))
+              }
+            })
+            break
+          case GRAPH_IMAGE:
+            session.send(h.image(result.display_url))
+            break
+          case GRAPH_VIDEO:
+            session.send(h.video(result.video_url))
+            break
+        }
+      }
+    } catch(err) {
+      console.log(err);
+      return `发生错误!;  ${err}`;
+    }
+  })
 }
